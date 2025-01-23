546952c960533a43b1bc92b0e3a8dabe2526d281
mihirkadam19 <mihirkadam85@yahoo.in>
2025-01-23 14:53:13 -0500

    first post method to create new products

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,39 @@
 //const express = require('express');
 import express from 'express';
 import {connectDB} from './config/db.js';
+import Product from './models/product.model.js';
 
 const app = express();
 
-app.get("/", (req,res) => {
-    res.send("Server is ready!");
+app.use(express.json()); // allows us to accept JSON data from req.body
+
+app.post("/api/createproducts", async (req,res) => {
+    const product = req.body; // User api request body
+
+    //check if the request body is valid
+    if (!product.name || !product.price || !product.image){
+        return res.status(400).json({
+            success: false,
+            message: "Please enter all the fields"
+        });
+    }
+
+    const newProduct = new Product(product);
+
+    try{
+        await newProduct.save();
+        return res.status(201).json({
+            success:true,
+            message: "New product added"
+        });
+    } catch (error){
+        console.error("Error creating new Product", error.message);
+        return res.status(500).json({
+            success:false,
+            message: "Server Error, could not create a new product"
+        })
+    }
+
 });
 
 app.listen(5000, () => {
