a71cdc87308acfc8e003a9f0e3cba9ba06c15c62
mihirkadam19 <mihirkadam85@yahoo.in>
2025-01-23 12:47:30 -0500

    bke config for mongo db

---
+++ backend/config/db.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config(); // loading the variables from the .env file
+
+export const connectDB = async () => {
+    try{
+        const conn = await mongoose.connect(process.env.MONGO_URI); //using the MONGO_URI from the .env file as connection string
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    }
+    catch (error){
+        console.error(`Error: $(error.message)`);
+        process.exit(1); //1 means error, 0 means success
+    }
+};
--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,18 @@
 //const express = require('express');
 import express from 'express';
 import dotenv from 'dotenv';
+import {connectDB} from './config/db.js';
 
 const app = express();
 
-dotenv.config();
-console.log(process.env.MONGO_URI);
+dotenv.config(); // loading the variables from the .env file
+
 
 app.get("/", (req,res) => {
     res.send("Server is ready!");
 });
 
 app.listen(5000, () => {
+    connectDB();
     console.log("Server started at http://localhost:5000");
 });
