4b1b5e81a2221e0798d36406f58b9ad836b7671f
mihirkadam19 <mihirkadam85@yahoo.in>
2025-01-23 14:14:00 -0500

    monogdb dependecy --

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,8 @@
 //const express = require('express');
 import express from 'express';
-import dotenv from 'dotenv';
 import {connectDB} from './config/db.js';
 
 const app = express();
-
-dotenv.config(); // loading the variables from the .env file
-
 
 app.get("/", (req,res) => {
     res.send("Server is ready!");
