eefdaad9a233b7c6b2c3e1a7604836f018aac3fe
mihirkadam19 <mihirkadam85@yahoo.in>
2025-01-23 12:31:02 -0500

    db init

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 //const express = require('express');
 import express from 'express';
+import dotenv from 'dotenv';
 
 const app = express();
+
+dotenv.config();
+console.log(process.env.MONGO_URI);
 
 app.get("/", (req,res) => {
     res.send("Server is ready!");
