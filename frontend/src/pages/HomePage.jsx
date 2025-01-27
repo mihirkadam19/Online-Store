2eccb252583fb6e6dc2d722989039ea5f56ca4c4
mihirkadam19 <mihirkadam85@yahoo.in>
2025-01-27 00:13:18 -0500

    sample grid

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import { Container, Flex, Icon, Text, VStack, SimpleGrid, Box, useColorMode } from '@chakra-ui/react';
+import { color } from 'framer-motion';
+import React from 'react';
+import { MdOutlineStorefront } from "react-icons/md";
 
 const HomePage = () => {
+    const {colorMode, toggleColorMode} = useColorMode();
   return (
-    <div>
-      Home Page
-    </div>
-  )
+    <Container maxW={"container.md"}>
+        <VStack >
+            <Flex
+                align={"center"}
+                justify={"center"}
+                gap={2}
+            >
+                <Text
+                    fontSize={{ base: "22", sm: "28" }}
+                    fontWeight={"bold"}
+                    textTransform={"uppercase"}
+                    textAlign={"center"}
+                    bgGradient={"linear(to-r, blue.500, green.300, cyan.400)"}
+                    bgClip={"text"}
+                    _hover={{
+                        bgGradient:"linear(to-r, cyan.700, green.500, blue.400)"
+                    }}
+                >
+                    Current Products
+                </Text>
+                <Icon as={MdOutlineStorefront} fontSize={27} color="cyan.400" _hover={{color: "blue.400"}} />
+            </Flex>
+        </VStack>
+        <SimpleGrid mt={6} columns={2} spacingX='30px' spacingY='30px'>
+            <Box bg = {colorMode=='light' ? "gray.400" : "blue.800"}
+                height='140px' borderRadius={"md"}></Box>
+        </SimpleGrid>
+    </Container>
+  );
 }
 
 export default HomePage;
