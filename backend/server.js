8ab03250814b16a228d3225ad7da8560d4cbbcb7
mihirkadam19 <mihirkadam85@yahoo.in>
2025-01-27 19:30:50 -0500

    deployment changes

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,22 @@
 import express from 'express';
 import {connectDB} from './config/db.js';
 import productRoutes from './routes/product.route.js';
+import path from "path";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // allows us to accept JSON data from req.body
 
-app.use("/api/products", productRoutes)
+app.use("/api/products", productRoutes);
+
+if (process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"/frontend/dist")));
+    
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    })
+}
 
 app.listen(PORT, () => {
     connectDB();
