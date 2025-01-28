c9b6aec4e8ce4f964a007666cbc3345394342987
mihirkadam19 <mihirkadam85@yahoo.in>
2025-01-27 20:10:19 -0500

    meta url

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,10 @@ import express from 'express';
 import {connectDB} from './config/db.js';
 import productRoutes from './routes/product.route.js';
 import path from "path";
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 5000;
